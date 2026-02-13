837c662f3d5d27ff57b9f670f3b7244ff2c08b42
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2026-02-13 17:12:05 +0100

    Fix license headers on the whole project

--- admin-dev/themes/new-theme/webpack.config.js
+++ admin-dev/themes/new-theme/webpack.config.js
@@ -1,3 +1,7 @@
+/**
+ * For the full copyright and license information, please view the
+ * docs/licenses/LICENSE.txt file that was distributed with this source code.
+ */
 /* eslint-disable indent,comma-dangle */
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
