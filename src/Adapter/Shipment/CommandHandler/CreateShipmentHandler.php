ca1a5e6e559c4bd314a808a0efb0a8c2a0b3f710
PoulainMaxime <mpoulain91800@gmail.com>
2026-02-24 17:51:18 +0100

    set shippingCost at 0

--- src/Adapter/Shipment/CommandHandler/CreateShipmentHandler.php
+++ src/Adapter/Shipment/CommandHandler/CreateShipmentHandler.php
@@ -9,29 +9,13 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Adapter\Shipment\CommandHandler;
 
 use Exception;
-use Order;
-use PrestaShop\PrestaShop\Adapter\Address\Repository\AddressRepository;
-use PrestaShop\PrestaShop\Adapter\Carrier\ShippingCostCalculator;
-use PrestaShop\PrestaShop\Adapter\Country\Repository\CountryRepository;
-use PrestaShop\PrestaShop\Adapter\Currency\Repository\CurrencyRepository;
 use PrestaShop\PrestaShop\Adapter\Order\Repository\OrderRepository;
-use PrestaShop\PrestaShop\Adapter\Product\Combination\Repository\CombinationRepository;
-use PrestaShop\PrestaShop\Adapter\Product\Repository\ProductRepository;
-use PrestaShop\PrestaShop\Adapter\Shop\Context as ShopContext;
 use PrestaShop\PrestaShop\Core\CommandBus\Attributes\AsCommandHandler;
-use PrestaShop\PrestaShop\Core\Domain\Address\ValueObject\AddressId;
-use PrestaShop\PrestaShop\Core\Domain\Carrier\ValueObject\ShippingCalculationRequest;
-use PrestaShop\PrestaShop\Core\Domain\Country\ValueObject\CountryId;
-use PrestaShop\PrestaShop\Core\Domain\Currency\ValueObject\CurrencyId;
-use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\CombinationId;
-use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
 use PrestaShop\PrestaShop\Core\Domain\Shipment\Command\CreateShipment;
 use PrestaShop\PrestaShop\Core\Domain\Shipment\CommandHandler\CreateShipmentHandlerInterface;
 use PrestaShop\PrestaShop\Core\Domain\Shipment\Exception\ShipmentException;
-use PrestaShop\PrestaShop\Core\Domain\Shop\ValueObject\ShopId;
 use PrestaShopBundle\Entity\Repository\ShipmentRepository;
 use PrestaShopBundle\Entity\Shipment;
-use Throwable;
 
 #[AsCommandHandler]
 class CreateShipmentHandler implements CreateShipmentHandlerInterface
@@ -39,13 +23,6 @@ class CreateShipmentHandler implements CreateShipmentHandlerInterface
     public function __construct(
         private readonly ShipmentRepository $shipmentRepository,
         private readonly OrderRepository $orderRepository,
-        private readonly AddressRepository $addressRepository,
-        private readonly ProductRepository $productRepository,
-        private readonly CountryRepository $countryRepository,
-        private readonly ShippingCostCalculator $shippingCostCalculator,
-        private readonly ShopContext $shopContext,
-        private readonly CurrencyRepository $currencyRepository,
-        private readonly CombinationRepository $combinationRepository,
     ) {
     }
 
@@ -54,20 +31,15 @@ class CreateShipmentHandler implements CreateShipmentHandlerInterface
         try {
             $order = $this->orderRepository->get($command->getOrderId());
             $carrierId = $command->getCarrierId()->getValue();
-            $productId = $command->getProductId();
-            $quantity = $command->getQuantity();
-            $combinationId = $command->getProductCombinationId();
 
             $shipment = new Shipment();
             $shipment->setOrderId((int) $order->id);
             $shipment->setCarrierId((int) $carrierId);
             $shipment->setAddressId((int) $order->id_address_delivery);
             $shipment->setTrackingNumber(null);
-
-            $shippingCosts = $this->calculateShippingCosts($order, $carrierId, $productId, $quantity, $combinationId);
-            $shipment->setShippingCostTaxExcluded($shippingCosts['tax_excluded']);
-            $shipment->setShippingCostTaxIncluded($shippingCosts['tax_included']);
-
+            // waiting for shippingCostCalculator, now we just set 0
+            $shipment->setShippingCostTaxExcluded(0.00);
+            $shipment->setShippingCostTaxIncluded(0.00);
             $shipment->setDeliveredAt(null);
             $shipment->setShippedAt(null);
             $shipment->setCancelledAt(null);
@@ -75,77 +47,6 @@ class CreateShipmentHandler implements CreateShipmentHandlerInterface
             return $this->shipmentRepository->save($shipment);
         } catch (Exception $e) {
             throw new ShipmentException('Failed to create shipment', $e->getCode(), $e);
-        }
-    }
-
-    /**
-     * @return array{tax_excluded: float, tax_included: float}
-     */
-    private function calculateShippingCosts(Order $order, int $carrierId, ProductId $productId, int $quantity, ?CombinationId $combinationId): array
-    {
-        try {
-            $shopId = new ShopId($this->shopContext->getContextShopID());
-            $product = $this->productRepository->get($productId, $shopId);
-            $attributeWeight = null;
-            $productPrice = (float) $product->price;
-
-            if (empty($combinationId->getValue())) {
-                $combination = $this->combinationRepository->get($combinationId, $shopId);
-                $attributeWeight = (float) $product->weight + (float) $combination->weight;
-                $productPrice += (float) $combination->price;
-            }
-
-            $productArray = [
-                'id_product' => (int) $product->id,
-                'id_product_attribute' => $combinationId->getValue(),
-                'quantity' => $quantity,
-                'weight' => (float) $product->weight,
-                'weight_attribute' => $attributeWeight,
-                'is_virtual' => (bool) $product->is_virtual,
-                'additional_shipping_cost' => (float) $product->additional_shipping_cost,
-                'price_wt' => $productPrice,
-            ];
-
-            $products = [$productArray];
-
-            $address = $this->addressRepository->get(new AddressId((int) $order->id_address_delivery));
-            $country = $this->countryRepository->get(new CountryId((int) $address->id_country));
-            $currency = $this->currencyRepository->get(new CurrencyId((int) $order->id_currency));
-
-            $orderTotal = $productPrice * $quantity;
-
-            $request = new ShippingCalculationRequest(
-                $products,
-                $carrierId,
-                null,
-                (int) $order->id_address_delivery,
-                (int) $country->id_zone,
-                (int) $order->id_currency,
-                (int) $order->id_customer,
-                $orderTotal
-            );
-
-            $result = $this->shippingCostCalculator->calculate($request);
-
-            if ($result === null) {
-                return [
-                    'tax_excluded' => 0.0,
-                    'tax_included' => 0.0,
-                ];
-            }
-
-            $taxExcluded = $result->getTaxExcluded();
-            $taxIncluded = $result->getTaxIncluded();
-
-            return [
-                'tax_excluded' => (float) $taxExcluded->round($currency->precision),
-                'tax_included' => (float) $taxIncluded->round($currency->precision),
-            ];
-        } catch (Throwable $e) {
-            return [
-                'tax_excluded' => 0.0,
-                'tax_included' => 0.0,
-            ];
         }
     }
 }
