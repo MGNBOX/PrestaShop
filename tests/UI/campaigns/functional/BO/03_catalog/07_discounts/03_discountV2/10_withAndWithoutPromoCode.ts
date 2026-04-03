745c4ea298a7da7d3b05ee6b256cd0cc4645b9a0
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-04-03 11:16:46 +0200

    Fix double steps id

--- tests/UI/campaigns/functional/BO/03_catalog/07_discounts/03_discountV2/10_withAndWithoutPromoCode.ts
+++ tests/UI/campaigns/functional/BO/03_catalog/07_discounts/03_discountV2/10_withAndWithoutPromoCode.ts
@@ -19,7 +19,6 @@ import {
   // Data
   dataProducts,
   FakerDiscount,
-  utilsCore,
   type BrowserContext,
   type Page,
   utilsPlaywright,
@@ -84,7 +83,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     discountReductionType: '€',
     discountTax: 'Tax included',
     generateDiscountCode: true,
-    createRandomCode: true,
+    generateRandomCode: true,
   });
 
   before(async function () {
@@ -210,7 +209,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should check the discount value', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountValue1', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountValue_1', baseContext);
 
       const discount = parseFloat(discountData.discountValue.toString()).toFixed(2);
 
@@ -222,7 +221,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should check the shipping cost', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkShippingCost_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkShippingCost_1', baseContext);
 
       const subTotalShipping = await foHummingbirdCartPage.getSubtotalShippingValue(page);
       expect(subTotalShipping).to.equal('Free');
@@ -237,7 +236,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
       expect(totalAfterDiscount.toFixed(2)).to.equal((dataProducts.demo_6.combinations[0].price - discount).toFixed(2));
     });
 
-    it('should delete the product from the cart', async function(){
+    it('should delete the product from the cart', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'deleteProductFromCart', baseContext);
 
       await foHummingbirdCartPage.deleteProduct(page, 1);
@@ -246,14 +245,14 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
       expect(notificationNumber).to.be.equal(0);
     });
 
-    it('should close the page', async function(){
+    it('should close the page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'closeFoPage', baseContext);
 
       page = await foHummingbirdHomePage.closePage(browserContext, page, 0);
 
       const pageTitle = await boDiscountsCreatePage.getPageTitle(page);
       expect(pageTitle).to.contains(boDiscountsCreatePage.pageTitle);
-    })
+    });
   });
 
   describe('Edit discount in BO and check it in FO - Add promo code', async () => {
@@ -304,7 +303,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should add the promo code and check the discount name', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'addPromoCode_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'addPromoCode_1', baseContext);
 
       await foHummingbirdCartPage.addPromoCode(page, editDiscountData.discountCode);
 
@@ -313,18 +312,18 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should check that the discount is applied', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountValue1', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountIsApplied', baseContext);
 
       const cartRuleName = await foHummingbirdCartPage.getCartRuleName(page, 1);
-      expect(cartRuleName).to.contains(discountData.name);
+      expect(cartRuleName).to.contains(editDiscountData.name);
 
-      const discount = parseFloat(discountData.discountValue.toString()).toFixed(2);
+      const discount = parseFloat(editDiscountData.discountValue.toString()).toFixed(2);
 
       const discountValue = await foHummingbirdCartPage.getCartRuleValue(page);
       expect(discountValue).to.contains(`-€${discount}`);
 
       const subTotalDiscount = await foHummingbirdCartPage.getSubtotalDiscountValue(page);
-      expect(subTotalDiscount.toString()).to.equal(`-${discountData.discountValue}`);
+      expect(subTotalDiscount.toString()).to.equal(`-${editDiscountData.discountValue}`);
     });
 
     it('should check the shipping cost', async function () {
@@ -335,16 +334,16 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should check the Total (tax incl.) after the discount', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkTotalAfterDiscount_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkTotalAfterDiscount_1', baseContext);
 
-      const discount = parseFloat(discountData.discountValue.toString());
+      const discount = parseFloat(editDiscountData.discountValue.toString());
 
       const totalAfterDiscount = await foHummingbirdCartPage.getATIPrice(page);
       expect(totalAfterDiscount.toFixed(2)).to.equal((dataProducts.demo_6.combinations[0].price - discount).toFixed(2));
     });
 
-    it('should delete the product from the cart', async function(){
-      await testContext.addContextItem(this, 'testIdentifier', 'deleteProductFromCart', baseContext);
+    it('should delete the product from the cart', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'deleteProductFromCart_2', baseContext);
 
       await foHummingbirdCartPage.deleteProduct(page, 1);
 
@@ -352,21 +351,21 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
       expect(notificationNumber).to.be.equal(0);
     });
 
-    it('should close the page', async function(){
+    it('should close the page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'closeFoPage_2', baseContext);
 
       page = await foHummingbirdHomePage.closePage(browserContext, page, 0);
 
       const pageTitle = await boDiscountsCreatePage.getPageTitle(page);
       expect(pageTitle).to.contains(boDiscountsCreatePage.pageTitle);
-    })
+    });
   });
 
   describe('Edit discount in BO and check it in FO - Generate random promo code', async () => {
     it('should edit the discount', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'editDiscount', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'editDiscount_2', baseContext);
 
-      const errorMessage = await boDiscountsCreatePage.createDiscount(page, editDiscountData);
+      const errorMessage = await boDiscountsCreatePage.createDiscount(page, discountRandomPromoCodeData);
       expect(errorMessage).to.contains(boDiscountsCreatePage.successfulUpdateMessage);
     });
 
@@ -375,15 +374,15 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
 
       const code = await boDiscountsCreatePage.getDiscountCode(page);
 
-      const errorMessage = await boDiscountsCreatePage.createDiscount(page, editDiscountData);
-      expect(errorMessage).to.contains(boDiscountsCreatePage.successfulUpdateMessage);
+      const successMessage = await boDiscountsCreatePage.createDiscount(page, discountRandomPromoCodeData);
+      expect(successMessage).to.contains(boDiscountsCreatePage.successfulUpdateMessage);
 
-      randomPromoCode= await boDiscountsCreatePage.getDiscountCode(page);
+      randomPromoCode = await boDiscountsCreatePage.getDiscountCode(page);
       expect(code).not.equal(randomPromoCode);
     });
 
     it('should view my shop', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'viewMyShop_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'viewMyShop_1', baseContext);
 
       page = await boDiscountsCreatePage.viewMyShop(page);
       await foHummingbirdHomePage.changeLanguage(page, 'en');
@@ -393,7 +392,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it(`should search for the product '${dataProducts.demo_6.name}'`, async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'searchProduct_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'searchProduct_3', baseContext);
 
       await foHummingbirdHomePage.searchProduct(page, dataProducts.demo_6.name);
 
@@ -402,7 +401,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it(`should add the product '${dataProducts.demo_6.name}' to the cart`, async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart_3', baseContext);
 
       await foHummingbirdSearchResultsPage.quickViewProduct(page, 1);
       await foHummingbirdModalQuickViewPage.addToCartByQuickView(page);
@@ -413,7 +412,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should check that the discount is not applied automatically', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountNotApplied', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountNotApplied_2', baseContext);
 
       await foHummingbirdCartPage.reloadPage(page);
 
@@ -431,22 +430,22 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     });
 
     it('should check that the discount is applied', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountValue1', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountIsApplied_2', baseContext);
 
       const cartRuleName = await foHummingbirdCartPage.getCartRuleName(page, 1);
-      expect(cartRuleName).to.contains(discountData.name);
+      expect(cartRuleName).to.contains(discountRandomPromoCodeData.name);
 
-      const discount = parseFloat(discountData.discountValue.toString()).toFixed(2);
+      const discount = parseFloat(discountRandomPromoCodeData.discountValue.toString()).toFixed(2);
 
       const discountValue = await foHummingbirdCartPage.getCartRuleValue(page);
       expect(discountValue).to.contains(`-€${discount}`);
 
       const subTotalDiscount = await foHummingbirdCartPage.getSubtotalDiscountValue(page);
-      expect(subTotalDiscount.toString()).to.equal(`-${discountData.discountValue}`);
+      expect(subTotalDiscount.toString()).to.equal(`-${discountRandomPromoCodeData.discountValue}`);
     });
 
     it('should check the shipping cost', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkShippingCost_2', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkShippingCost_3', baseContext);
 
       const subTotalShipping = await foHummingbirdCartPage.getSubtotalShippingValue(page);
       expect(subTotalShipping).to.equal('Free');
@@ -455,7 +454,7 @@ describe('BO - Catalog - Discounts : Create a discount that applies automaticall
     it('should check the Total (tax incl.) after the discount', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'checkTotalAfterDiscount_2', baseContext);
 
-      const discount = parseFloat(discountData.discountValue.toString());
+      const discount = parseFloat(discountRandomPromoCodeData.discountValue.toString());
 
       const totalAfterDiscount = await foHummingbirdCartPage.getATIPrice(page);
       expect(totalAfterDiscount.toFixed(2)).to.equal((dataProducts.demo_6.combinations[0].price - discount).toFixed(2));
